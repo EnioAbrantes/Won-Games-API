33183aaa149dfde665d8f3d8256097ab87173b15
Ênio <enio.abrantes.silva@gmail.com>
2021-07-12 08:27:31 -0300

    Add create Games

--- api/api/game/services/game.js
+++ api/api/game/services/game.js
@@ -13,9 +13,18 @@ async function getGameInfo(slug) {
   const body = await axios.get(`https://www.gog.com/game/${slug}`)
   const dom = new JSDOM(body.data)
 
+  const ratingElement = dom.window.document.querySelector(
+    ".age-restrictions__icon use"
+  );
+
   const description = dom.window.document.querySelector('.description')
   return {
-    rating: 'BR0',
+    rating: ratingElement
+      ? ratingElement
+          .getAttribute("xlink:href")
+          .replace(/_/g, "")
+          .replace(/[^\w-]+/g, "")
+      : "FREE",
     short_description: description.textContent.slice(0, 160),
     description: description.innerHTML
 
@@ -51,6 +60,40 @@ async function createManyToManyData(products) {
   ]);
 }
 
+async function createGames(products) {
+  await Promise.all(
+    products.map(async (product) => {
+      const item = await getByName(product.title, "game");
+
+      if (!item) {
+        console.info(`Creating: ${product.title}...`);
+
+        const game = await strapi.services.game.create({
+          name: product.title,
+          slug: product.slug.replace(/_/g, "-"),
+          price: product.price.amount,
+          release_date: new Date(
+            Number(product.globalReleaseDate) * 1000
+          ).toISOString(),
+          categories: await Promise.all(
+            product.genres.map((name) => getByName(name, "category"))
+          ),
+          platforms: await Promise.all(
+            product.supportedOperatingSystems.map((name) =>
+              getByName(name, "platform")
+            )
+          ),
+          developers: [await getByName(product.developer, "developer")],
+          publisher: await getByName(product.publisher, "publisher"),
+          ...(await getGameInfo(product.slug)),
+        });
+
+        return game;
+      }
+    })
+  );
+}
+
 async function getByName(name, entityName){
   const item = await strapi.services[entityName].find({ name });
   return item.length ? item[0] : null;
@@ -74,6 +117,7 @@ module.exports = {
     const {data : { products }} = await axios.get(gogApiUrl)
 
     await createManyToManyData([products[3], products[4]])
+    await createGames([products[3], products[4]])
     // console.log(await getByName('CD PROJECT RED', 'developer'))
   }
 };
